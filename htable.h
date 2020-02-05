2fd7a462125192ca8ac68472e3882957dacd02be
baheithold <baheithold@crimson.ua.edu>
2020-02-05 00:35:54 -0600

    Created HTABLE interface

--- htable.h
+++ htable.h
@@ -6,6 +6,24 @@
 #ifndef __HTABLE_INCLUDED__
 #define __HTABLE_INCLUDED__
 
+#include <stdio.h>
 
+typedef struct HTABLE HTABLE;
 
-#endif // !__HTABLE_INCLUDED
+extern HTABLE *newHTABLE(void);
+extern void    setHTABLEdisplay(HTABLE *table, void (*display)(void *, FILE *));
+extern void    setHTABLEfree(HTABLE *table, void (*free)(void *));
+extern void    insertHTABLE(HTABLE *table, void *key, void *value);
+extern void   *removeHTABLE(HTABLE *table, void *key);
+extern int     findKey(HTABLE *table, void *key);
+extern void   *getHTABLEvalue(HTABLE *table, void *key);
+extern void    clearHTABLE(HTABLE *table);
+extern int     containsKey(HTABLE *table, void *key);
+extern int     containsValue(HTABLE *table, void *value);
+extern int     isHTABLEempty(HTABLE *table);
+extern int     sizeHTABLE(HTABLE *table);
+extern void    displayHTABLE(HTABLE *table, FILE *fp);
+extern int     debugHTABLE(HTABLE *table, int level);
+extern void    freeHTABLE(HTABLE *table);
+
+#endif // !__HTABLE_INCLUDED__
