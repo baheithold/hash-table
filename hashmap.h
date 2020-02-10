78423e9be7994381ee3b1f7ad95c90c1608aa5db
baheithold <baheithold@crimson.ua.edu>
2020-02-09 22:35:07 -0600

    Refactored

--- hashmap.h
+++ hashmap.h
@@ -7,6 +7,7 @@
 #ifndef __HASHMAP_INCLUDED__
 #define __HASHMAP_INCLUDED__
 
+#include <stdbool.h>
 #include <stdio.h>
 
 typedef struct HASHMAP HASHMAP;
@@ -19,12 +20,10 @@ extern void    setHASHMAPfreeValue(HASHMAP *map, void (*free)(void *));
 extern double  setHASHMAPLoadFactor(HASHMAP *map, double loadFactor);
 extern void    insertHASHMAP(HASHMAP *map, void *key, void *value);
 extern void   *removeHASHMAP(HASHMAP *map, void *key);
-extern int     findKey(HASHMAP *map, void *key);
 extern void   *getHASHMAPvalue(HASHMAP *map, void *key);
 extern void    clearHASHMAP(HASHMAP *map);
-extern int     containsKey(HASHMAP *map, void *key);
-extern int     containsValue(HASHMAP *map, void *value);
-extern int     isHASHMAPempty(HASHMAP *map);
+extern bool    containsKey(HASHMAP *map, void *key);
+extern bool    isHASHMAPempty(HASHMAP *map);
 extern int     sizeHASHMAP(HASHMAP *map);
 extern void    displayHASHMAP(HASHMAP *map, FILE *fp);
 extern int     debugHASHMAP(HASHMAP *map, int level);
